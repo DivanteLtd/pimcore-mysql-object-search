0d5f9419e51c01ac0ae0ae9d6a95b293449606f0
Bruno Ramalho <bfmramalho@gmail.com>
2019-12-22 21:40:26 +0100

    install ci

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,8 @@ set -eu
 PROJECT_DIR="$( cd "$(dirname "$0")" ; pwd -P )/../tmp"
 DEPENDENCIES="$( cd "$(dirname "$0")" ; pwd -P )/dependencies.txt"
 
-PACKAGE_NAME="ScheduledExportBundle"
-BUNDLE_NAME="DivanteScheduledExportBundle"
+PACKAGE_NAME="AdvancedSearchBundle"
+BUNDLE_NAME="AdvancedSearchBundle"
 
 CREATE_DB=${CREATE_DB-true}
 DB_HOST=${DB_HOST-localhost}
