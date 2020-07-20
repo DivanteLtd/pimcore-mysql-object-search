5a72cda5f81193607ce4d77c8248a753d75ebd60
Pascal Dunaj <pdunaj@divante.pl>
2020-07-20 15:08:06 +0200

    Related field dropzone

--- src/AdvancedSearchBundle/Resources/public/js/pimcore/searchConfig/fieldConditionPanel/manyToManyOne.js
+++ src/AdvancedSearchBundle/Resources/public/js/pimcore/searchConfig/fieldConditionPanel/manyToManyOne.js
@@ -40,12 +40,43 @@ pimcore.bundle.advancedSearch.searchConfig.fieldConditionPanel.manyToOneRelation
                     change: function( item, newValue, oldValue, eOpts ) {
                         this.subPanel.removeAll();
                         if(newValue != "object_filter") {
-
                             this.idsField = Ext.create('Ext.form.field.Text',
                                 {
                                     fieldLabel:  t("bundle_advancedSearch_ids"),
                                     width: 400,
-                                    value: this.data.filterEntryData && this.data.filterEntryData.id ? this.data.filterEntryData.id.join() : ""
+                                    value: this.data.filterEntryData && this.data.filterEntryData.id ? this.data.filterEntryData.id.join() : "",
+                                    listeners: {
+                                        render: function (el) {
+                                            new Ext.dd.DropZone(el.getEl(), {
+                                                reference: this,
+                                                ddGroup: 'element',
+                                                getTargetFromEvent: function (e) {
+                                                    return this.getEl();
+                                                }.bind(el),
+
+                                                onNodeOver: function (target, dd, e, data) {
+                                                    data = data.records[0].data;
+
+                                                    if (data.elementType == 'object') {
+                                                        return Ext.dd.DropZone.prototype.dropAllowed;
+                                                    }
+
+                                                    return Ext.dd.DropZone.prototype.dropNotAllowed;
+                                                },
+
+                                                onNodeDrop: function (target, dd, e, data) {
+                                                    data = data.records[0].data;
+
+                                                    if (data.elementType == 'object') {
+                                                        this.setValue(data.id);
+                                                        return true;
+                                                    }
+
+                                                    return false;
+                                                }.bind(el)
+                                            });
+                                        }
+                                    }
                                 }
                             );
 
