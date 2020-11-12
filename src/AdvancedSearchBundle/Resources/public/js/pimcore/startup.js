51c3f847297aa808bdc8e577979f17687ab13a01
Pascal Dunaj <pdunaj@divante.pl>
2020-11-12 11:29:14 +0100

    Permissions fix

--- src/AdvancedSearchBundle/Resources/public/js/pimcore/startup.js
+++ src/AdvancedSearchBundle/Resources/public/js/pimcore/startup.js
@@ -12,13 +12,10 @@ pimcore.bundle.advancedSearch = Class.create(pimcore.plugin.admin, {
     pimcoreReady: function (params, broker) {
         var perspectiveCfg = pimcore.globalmanager.get("perspective");
         var user = pimcore.globalmanager.get("user");
-
-        if (user.roles.length === 0) {
-            var searchMenu = pimcore.globalmanager.get("layout_toolbar").searchMenu;
-            if (searchMenu && perspectiveCfg.inToolbar("search.advancedSearch")) {
-                Ext.get('pimcore_menu_search').insertSibling('<li id="pimcore_menu_advanced_search" data-menu-tooltip="Advanced search" class="pimcore_bundle_nav_icon_advancedSearch pimcore_menu_item pimcore_menu_needs_children"></li>', 'after');
-                pimcore.bundle.advancedSearch.helper.rebuildAdvancedSearchMenu();
-            }
+        var searchMenu = pimcore.globalmanager.get("layout_toolbar").searchMenu;
+        if (searchMenu && perspectiveCfg.inToolbar("search.advancedSearch")) {
+            Ext.get('pimcore_menu_search').insertSibling('<li id="pimcore_menu_advanced_search" data-menu-tooltip="Advanced search" class="pimcore_bundle_nav_icon_advancedSearch pimcore_menu_item pimcore_menu_needs_children"></li>', 'after');
+            pimcore.bundle.advancedSearch.helper.rebuildAdvancedSearchMenu();
         }
     }
 });
